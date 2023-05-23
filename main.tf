56c61390748376cff5163b994dc9eb68d2ff3254
bgFromDummy <130542883+bgFromDummy@users.noreply.github.com>
2023-05-23 12:12:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,4 @@
-    # The configuration for the `remote` backend.
-    terraform {
-      backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "example-organization"
-
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-          name = "example-workspace"
-        }
-      }
-    }
-
-    # An example resource that does nothing.
-    resource "null_resource" "example" {
-      triggers = {
-        value = "A example resource that does nothing!"
-      }
-    }
+resource "local_file" "foo" {
+  content  = "foo!"
+  filename = "./foo.bar"
+}
