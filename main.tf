b9f9289072448021102c892489b3fad61a975560
bgFromDummy <130542883+bgFromDummy@users.noreply.github.com>
2023-05-23 18:39:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "local_file" "foo" {
   content  = "foo!"
   filename = "./foo.bar"
 }
+
+resource "local_sensitive_file" "foo1" {
+  content  = "foo!"
+  filename = "${path.module}/foo1.bar"
+}
